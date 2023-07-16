dc3634db12f48b256cc898e7c214c14ce0dca32a
smoBEE <z94624@gmail.com>
2023-07-16 19:26:02 +0800

    Type Declaration File

---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+// 供全域使用
+
+interface Todo {
+    text: string,
+    complete: boolean
+}
+
+type AddTodo = (text: string) => void;
+
+type ToggleTodo = (index: number) => void;
+
+type DeleteTodo = (index: number) => void;
+
+/*
+ * Interfaces are better when you need to define a new object or method of an object.
+ * Types are better when you need to create functions.
+ */
