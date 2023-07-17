d1a83d0a344fcd574fde73b83e4b06dba23c5c0c
smoBEE <z94624@gmail.com>
2023-07-17 22:24:01 +0800

    TodoItem Component

---
+++ src/components/todos/TodoItem.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+interface Props {
+  todo: Todo;
+  index: number;
+  onToggleTodo: ToggleTodo;
+  onDeleteTodo: DeleteTodo;
+}
+
+// 定義 completed's style
+const StyledTodo = styled.span<{ completed: boolean }>`
+  text-decoration: ${(props) => (props.completed ? "line-through" : "none")};
+`;
+
+/* FC(Function Component)
+ * React.FC 會依照 Props 定義好的屬性型別帶入參數
+ */
+const TodoItem: React.FC<Props> = ({
+  todo,
+  index,
+  onToggleTodo,
+  onDeleteTodo,
+}) => {
+  return (
+    <div className="form-check border border-bottom-secondary rounded py-3 m-0 d-flex justify-content-between align-items-center">
+      <div>
+        <input
+          className="ms-1 me-3 form-check-input"
+          type="checkbox"
+          checked={todo.complete}
+          onClick={() => onToggleTodo && onToggleTodo(index)}
+        />
+        <StyledTodo className="todo" completed={todo.complete}>
+          {todo.text}
+        </StyledTodo>
+      </div>
+      <button
+        type="button"
+        className="btn btn-outline-danger me-3"
+        onClick={() => onDeleteTodo && onDeleteTodo(index)}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+
+export default TodoItem;
